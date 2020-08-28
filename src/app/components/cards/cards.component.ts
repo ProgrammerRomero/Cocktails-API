b6217a0c8127a2df40428edd21e08409650f6a8c
programmer <0xprogrammerx0@gmail.com>
2020-08-28 11:38:19 +1000

    Data called from Cocktails API, Printed into console, cards component

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
-export class CardsComponent implements OnInit {
+export class CardsComponent  {
 
-  constructor() { }
+  cocktailData: any[] = [];
 
-  ngOnInit(): void {
-  }
+  constructor( private http: HttpClient ) {
+
+    //Prepare to subscribe
+      this.http.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+          .subscribe( (resp: any) => {
+
+            this.cocktailData = resp;
+            console.log(this.cocktailData);
+          });
+   }
+
+
 
 }
